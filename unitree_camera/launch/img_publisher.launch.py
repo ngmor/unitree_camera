18689ee24a1f4a5ac3da8664c8e4cf89ee833dd3
Nick Morales <ngmorales97@gmail.com>
2023-02-19 20:26:27 -0600

    Added new package for ternary_text_substitution module

--- unitree_camera/launch/img_publisher.launch.py
+++ unitree_camera/launch/img_publisher.launch.py
@@ -22,58 +22,7 @@ from launch.conditions import IfCondition, LaunchConfigurationEquals
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.descriptions import ParameterValue
-# from .ternary_text_substitution import TernaryTextSubstitution
-
-from typing import Text
-
-from launch.condition import Condition
-from launch.substitution import Substitution
-from launch.launch_context import LaunchContext
-
-class TernaryTextSubstitution(Substitution):
-    """Substitution that returns the first text argument if the condition is true, otherwise it returns the second text argument."""
-    def __init__(self, condition: Condition, true_text: Text, false_text: Text) -> None:
-        """Create a TernaryTextSubstitution."""
-        super().__init__()
-
-        if not isinstance(true_text, Text):
-            raise TypeError(
-                "TernaryTextSubstitution expected Text object got '{}' instead.".format(type(true_text))
-            )
-        if not isinstance(false_text, Text):
-            raise TypeError(
-                "TernaryTextSubstitution expected Text object got '{}' instead.".format(type(false_text))
-            )
-        
-        if not isinstance(condition, Condition):
-            raise TypeError(
-                "TernaryTextSubstitution expected Condition object got '{}' instead.".format(type(condition))
-            )
-
-        self.__true_text = true_text
-        self.__false_text = false_text
-        self.__condition = condition
-
-    @property
-    def true_text(self) -> Text:
-        """Getter for true text."""
-        return self.__true_text
-    
-    @property
-    def false_text(self) -> Text:
-        """Getter for false text."""
-        return self.__false_text
-    
-    def describe(self) -> Text:
-        """Return a description of this substitution as a string."""
-        return f"'{self.true_text}' if true, '{self.false_text}' if false"
-
-    def perform(self, context: LaunchContext) -> Text:
-        """Perform the substitution by returning the string itself."""
-        if self.__condition.evaluate(context):
-            return self.true_text
-        else:
-            return self.false_text
+from unitree_camera_launch_module.ternary_text_substitution import TernaryTextSubstitution
 
 def generate_launch_description():
     """Launch the image publisher node for a certain specified camera."""
@@ -86,7 +35,7 @@ def generate_launch_description():
         ),
         SetLaunchConfiguration(
             name='dummy',
-            value='true'
+            value='false'
         ),
         SetLaunchConfiguration(
             name='yaml_file',

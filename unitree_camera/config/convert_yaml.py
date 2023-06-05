1d76b8f34263a836e13ea4bd1c5ce5400ead4984
Katie Hughes <katiereeshughes@gmail.com>
2023-06-05 12:09:22 -0500

    Into ros yaml format

--- unitree_camera/config/convert_yaml.py
+++ unitree_camera/config/convert_yaml.py
@@ -1,27 +1,25 @@
 
-base = 'head_front'
-
-
-with open(base+'.yaml', 'w') as f1:
-  for side in ['left', 'right']:
-    print(f"\n\nside={side}")
-    with open(base+'_'+side+'.yaml', 'r') as f2:
-      for line in f2.readlines():
-        # print(line.strip('\n'))
-        lastchar = line.strip('\n')[-1]
-        id = line.split()[0].strip(':')
-        id = id.strip()
-        print(f"ID: {id}")
-        # print(f"last char: {lastchar}")
-        if id != 'rows' and id != 'cols':
-          if id == 'data':
-            line = ' '+' '.join(line.split()[1:])
-            if lastchar == ']':
-              line += '\n'
-          if '_' in id:
-            line = side+'_'+line
-          if lastchar == ',' or id == 'camera_matrix' or id == 'projection_matrix' or id == 'rectification_matrix' or id == 'distortion_coefficients':
-            f1.write(line.strip('\n'))
-          else:
-            f1.write(line)
-        print()
+bases = ['head_front']
+for base in bases:
+  with open(base+'.yaml', 'w') as f1:
+    f1.write('/**\n')
+    f1.write('\tros__parameters:\n')
+    for side in ['left', 'right']:
+      with open(base+'_'+side+'.yaml', 'r') as f2:
+        for line in f2.readlines():
+          # print(line.strip('\n'))
+          lastchar = line.strip('\n')[-1]
+          id = line.split()[0].strip(':')
+          id = id.strip()
+          # print(f"last char: {lastchar}")
+          if id != 'rows' and id != 'cols':
+            if id == 'data':
+              line = ' '+' '.join(line.split()[1:])
+              if lastchar == ']':
+                line += '\n'
+            if '_' in id:
+              line = '\t\t'+side+'_'+line
+            if lastchar == ',' or id == 'camera_matrix' or id == 'projection_matrix' or id == 'rectification_matrix' or id == 'distortion_coefficients':
+              f1.write(line.strip('\n'))
+            else:
+              f1.write(line)
